a50b29adea262776a0d9e69da85bf0609eed9500
Yury03 <yrvish03@bk.ru>
2022-10-17 16:42:40 +0300

    add button and activity

--- app/src/main/java/com/example/weatherproject/MainActivity.java
+++ app/src/main/java/com/example/weatherproject/MainActivity.java
@@ -1,14 +1,22 @@
 
 package com.example.weatherproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button changeCity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeCity = findViewById(R.id.butChangeCity);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(this, city.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/weatherproject/RemoteFetch.java
+++ app/src/main/java/com/example/weatherproject/RemoteFetch.java
@@ -1,4 +1,50 @@
 package com.example.weatherproject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
 public class RemoteFetch {
+//
+//    private static final String OPEN_WEATHER_MAP_API =
+//            "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+//
+//    public static JSONObject getJSON(Context context, String city){
+//        try {
+//            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+//            HttpURLConnection connection =
+//                    (HttpURLConnection)url.openConnection();
+//
+//            connection.addRequestProperty("x-api-key",
+//                    context.getString(R.string.open_weather_maps_app_id));
+//
+//            BufferedReader reader = new BufferedReader(
+//                    new InputStreamReader(connection.getInputStream()));
+//
+//            StringBuffer json = new StringBuffer(1024);
+//            String tmp="";
+//            while((tmp=reader.readLine())!=null)
+//                json.append(tmp).append("\n");
+//            reader.close();
+//
+//            JSONObject data = new JSONObject(json.toString());
+//
+//            // This value will be 404 if the request was not
+//            // successful
+//            if(data.getInt("cod") != 200){
+//                return null;
+//            }
+//
+//            return data;
+//        }catch(Exception e){
+//            return null;
+//        }
+//    }
 }
+//https://code.tutsplus.com/ru/tutorials/create-a-weather-app-on-android--cms-21587
+//пункт 9
