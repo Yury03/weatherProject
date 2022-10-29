b4a82afaea35358df9b1ae24c77df22103ab872a
Yury Vishnevsky <yury@MacBook-Air-Yury.local>
2022-10-30 00:12:27 +0300

    обновлена кнопка сменить город

--- app/src/main/java/com/example/weatherproject/MainActivity.java
+++ app/src/main/java/com/example/weatherproject/MainActivity.java
@@ -4,21 +4,31 @@ package com.example.weatherproject;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private Button changeCity;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeCity = findViewById(R.id.butChangeCity);
+        textView = findViewById(R.id.textView);
+        Bundle arguments = getIntent().getExtras();
+        if(arguments!=null){
+            String cityVar = arguments.get("cityVar").toString();
+            textView.setText("Город: " + cityVar);
+        }
+
     }
     public void onClick(View view) {
         Intent intent = new Intent(this, city.class);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in, R.anim.left_out);
+        finish();
     }
 
 }
--- app/src/main/java/com/example/weatherproject/city.java
+++ app/src/main/java/com/example/weatherproject/city.java
@@ -2,21 +2,33 @@ package com.example.weatherproject;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class city extends AppCompatActivity {
-
+    String cityVar;
+    Button chCityBut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
+        chCityBut=findViewById(R.id.butChangeCity);
 
-
+    }
+    public void findCityBut(View view){
+        EditText editTextCity = findViewById(R.id.editTextCity);
+        cityVar = editTextCity.getText().toString();
+        finishThisActivity(chCityBut);
     }
     public void finishThisActivity(View view){
         Intent intent = new Intent(this, MainActivity.class);
-        finish();
+        intent.putExtra("cityVar", cityVar);
+        startActivity(intent);
         overridePendingTransition(R.anim.right_in, R.anim.right_out);
+        finish();
     }
 }
