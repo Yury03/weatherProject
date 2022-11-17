0f527a1c1c248fd57d72b829feb2084a4f757a1f
Yury Vishnevsky <yury@MacBook-Air-Yury.local>
2022-11-18 00:24:10 +0300

    add api

--- app/src/main/java/com/example/weatherproject/MainActivity.java
+++ app/src/main/java/com/example/weatherproject/MainActivity.java
@@ -2,6 +2,8 @@
 package com.example.weatherproject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,21 +11,34 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     private Button changeCity;
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
         Bundle arguments = getIntent().getExtras();
-        if(arguments!=null){
+        if (arguments != null) {
             String cityVar = arguments.get("cityVar").toString();
             textView.setText("Город: " + cityVar);
+            String api = "188293f79c0695b76bd873ec916a0f2f";
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityVar + "&appid=" + api + "&units=metric&lang=ru";
+            new getData().execute(url);
         }
 
     }
+
     public void onClick(View view) {
         Intent intent = new Intent(this, city.class);
         startActivity(intent);
@@ -31,4 +46,50 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    private class getData extends AsyncTask<String, String, String> {
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            nameTextView.setText("Ожидайте");для вывода в процессе загрузки
+//        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append(("\n"));
+                }
+                return buffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/weatherproject/city.java
+++ app/src/main/java/com/example/weatherproject/city.java
@@ -1,10 +1,11 @@
 package com.example.weatherproject;
-
+//188293f79c0695b76bd873ec916a0f2f api ключ
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.material.textfield.TextInputEditText;
@@ -12,17 +13,25 @@ import com.google.android.material.textfield.TextInputEditText;
 public class city extends AppCompatActivity {
     String cityVar;
     Button chCityBut;
+    //EditText CityText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
         chCityBut=findViewById(R.id.butChangeCity);
+        //CityText=findViewById(R.id.editTextCity);
 
     }
     public void findCityBut(View view){
         EditText editTextCity = findViewById(R.id.editTextCity);
         cityVar = editTextCity.getText().toString();
-        finishThisActivity(chCityBut);
+        //if(CityText.getText().toString().trim().equals("")){
+        if(cityVar.trim().equals("")){
+            Toast.makeText(city.this, R.string.noUserInput, Toast.LENGTH_LONG).show();
+        }else{
+            finishThisActivity(chCityBut);
+        }
+
     }
     public void finishThisActivity(View view){
         Intent intent = new Intent(this, MainActivity.class);
